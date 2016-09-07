bccad517f0ed7b5208064034aceb05db9c9d9831
sanam.dongol@gmail.com <sanam.dongol@gmail.com>
2016-09-07 13:41:09 +0545

    Toolbar Added in List

--- app/src/main/java/com/razio/MenuOne.java
+++ app/src/main/java/com/razio/MenuOne.java
@@ -1,8 +1,12 @@
 package com.razio;
 
+import android.app.ActionBar;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -12,18 +16,23 @@ import android.widget.ListView;
 /**
  * Created by sanam on 8/14/16.
  */
-public class MenuOne extends Activity {
+public class MenuOne extends AppCompatActivity {
 
     ListView listView;
     ArrayAdapter<CharSequence> adapter;
-
+    android.support.v7.app.ActionBar actionBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.fragment_home);
 
+        actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setHomeButtonEnabled(true);
+
         listView = (ListView) findViewById(R.id.listView);
+
 
         adapter = ArrayAdapter.createFromResource(this, R.array.nav_drawer_items, android.R.layout.simple_list_item_1);
         listView.setAdapter(adapter);
@@ -39,6 +48,17 @@ public class MenuOne extends Activity {
         });
 
 
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                onBackPressed();
+                finish();
+
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
 
